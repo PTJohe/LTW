2ed68ad217fe0d3ac4892bd997d2b549b1008871
ZeCarlosCoutinho <jczelik@gmail.com>
2016-11-26 19:10:02 +0000

    database: after inserting/deleting reviews, updates the restaurant's average score automatically

--- database.sql
+++ database.sql
@@ -7,6 +7,8 @@ DROP TABLE IF EXISTS restaurants;
 DROP TABLE IF EXISTS reviews;
 
 
+-- ######### CREATE TABLES #####################
+
 CREATE TABLE users (
 	idUser INTEGER PRIMARY KEY AUTOINCREMENT,
 	username NVARCHAR2(30) UNIQUE,
@@ -25,16 +27,21 @@ CREATE TABLE restaurants (
 	category NVARCHAR2(15),
 	-- imagelist dunno how to do it
 	lastUpdateDate DATE, 
-	idOwner NVARCHAR2(30) REFERENCES users(idUser) NOT NULL
+	idOwner INTEGER REFERENCES users(idUser) NOT NULL
 	);
 	
 CREATE TABLE reviews (
 	idReview INTEGER PRIMARY KEY AUTOINCREMENT,
-	idOwner INTEGER UNIQUE,
+	idOwner	INTEGER REFERENCES users(idUser) NOT NULL,
+	idRestaurant INTEGER REFERENCES restaurants(idRestaurant) NOT NULL,
 	text TEXT,
 	rating FLOAT
 	);
+
+
 	
+-- ######### INSERT USERS ##################
+
 INSERT INTO users(username, password, name)
 	VALUES("Maxzelik", "21ago96", "José Carlos Coutinho");
 INSERT INTO users(username, password, name)
@@ -42,7 +49,25 @@ INSERT INTO users(username, password, name)
 INSERT INTO users(username, password, name)
 	VALUES("Joao", "aquelapass", "João Araújo");
 	
--- TODO TRIGGERS TO CHANGE AVERAGE RATING
+	
+
+-- ######## TRIGGERS TO UPDATE AVERAGE RESTAURANT RATING ########
+CREATE TRIGGER updateAverageinsert AFTER INSERT ON reviews
+FOR EACH ROW
+BEGIN
+	UPDATE restaurants SET averageRating = 
+		(SELECT AVG(rating) FROM reviews WHERE reviews.idRestaurant = NEW.idRestaurant)
+		WHERE restaurants.idRestaurant = NEW.idRestaurant;
+END;
+
+CREATE TRIGGER updateAveragedelete AFTER DELETE ON reviews
+FOR EACH ROW
+BEGIN
+	UPDATE restaurants SET averageRating = 
+		(SELECT AVG(rating) FROM reviews WHERE reviews.idRestaurant = OLD.idRestaurant)
+		WHERE restaurants.idRestaurant = OLD.idRestaurant;
+END;
+
 INSERT INTO restaurants(restaurantName, address, contact, description, category, idOwner)
 	VALUES("Tasca do Bino", "Rua do Azevinho, 135", 223456789,
 	"A melhor tasca que alguma vez irás experimentar", "Tasco",
@@ -54,3 +79,15 @@ INSERT INTO restaurants(restaurantName, address, contact, description, category,
 	mas adequado para refeiçoes com classe", "Restaurante",
 	(SELECT idUser FROM users
 	WHERE username="Joao"));
+INSERT INTO reviews(idOwner, idRestaurant, text, rating)
+	VALUES(	(SELECT idUser FROM users WHERE username="Maxzelik"),
+			(SELECT idRestaurant FROM restaurants WHERE restaurantName="Restaurante Boavista"),
+			"Restaurante bastante bom, mas demasiado caro",
+			4);
+INSERT INTO reviews(idOwner, idRestaurant, text, rating)
+	VALUES(	(SELECT idUser FROM users WHERE username="Bolota"),
+			(SELECT idRestaurant FROM restaurants WHERE restaurantName="Restaurante Boavista"),
+			"Gostei bastante,gostaria de repetir a experiencia",
+			5);
+DELETE FROM reviews
+WHERE rating = 5;
