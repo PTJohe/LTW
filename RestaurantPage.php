d3c36e445d7a25c44e71ffb656b8d7f42f52e1ae
ZeCarlosCoutinho <jczelik@gmail.com>
2016-11-28 00:45:49 +0000

    RestaurantPage now shows the rest of the restaurant info, and the comments and responses.
    TODO:
    AddReview text box
    AddResponse text box
    EditMode for Owner

--- RestaurantPage.php
+++ RestaurantPage.php
@@ -26,8 +26,11 @@
 		$restaurantAddress = $selectedRestaurant['address'];
 		$restaurantContact = $selectedRestaurant['contact'];
 		$restaurantAverageRating = $selectedRestaurant['averageRating'];
+		$restaurantDescription = $selectedRestaurant['description'];
+		$restaurantCategory = $selectedRestaurant['category'];
 	?>
 	<head>
+		<meta charset="UTF-8">
 		<title><?=$restaurantName?></title>
 	</head>
 	
@@ -41,29 +44,43 @@
 			<p>Address: <?=$restaurantAddress?></p>
 			<p>Number: <?=$restaurantContact?></p>
 			<p>Rating: <?=$restaurantAverageRating?> / 5 </p>
+			<p><?=$restaurantDescription?></p>
+			<p>Category: <?=$restaurantCategory?></p>
 		</div>
-		
+		<br>
 		<div>
-			<ul>
+			<ul id="reviews">
 				<?php for($i = 0; $i < count($restaurantReviews); $i++) //WTF???? COM <?php funciona, sem o "php" ja nao funciona, e o contrário para as de baixo
-				{ ?>
-				<p>Rating: <?=$restaurantReviews[$i]['rating']?></p>
-				<li>
-					<?=$restaurantReviews[$i]['text']?>
-					<ul>
-						<?php 
-						$stmt = $dbh->prepare('SELECT * FROM responses WHERE idReview = ?');
-						$stmt->execute(array($restaurantReviews[$i]['idReview']));
-						$responses = $stmt->fetchAll();
-						for($j = 0; $j < count($responses); $j++)
-						{
-						?>
-						<li>
-							<?=$responses[$j]['text']?>
-						</li>
-						<?php } ?>
-					</ul>
-				</li>
+				{ 
+					//Gets the user who wrote the actual review
+					$stmt = $dbh->prepare('SELECT * FROM users WHERE idUser = ?');
+					$stmt->execute(array($restaurantReviews[$i]['idUser']));
+					$reviewUser = $stmt->fetch();
+					?>
+					<p>Rating: <?=$restaurantReviews[$i]['rating']?></p>
+					
+					<p>Written by <?=$reviewUser['name'];?></p>
+					<li>
+						<?=$restaurantReviews[$i]['text']?>
+						<ul>
+							<?php 
+							//Get the users who wrote the responses
+							$stmt = $dbh->prepare('SELECT * FROM responses WHERE idReview = ?');
+							$stmt->execute(array($restaurantReviews[$i]['idReview']));
+							$responses = $stmt->fetchAll();
+							for($j = 0; $j < count($responses); $j++)
+							{
+								$stmt = $dbh->prepare('SELECT * FROM users WHERE idUser = ?');
+								$stmt->execute(array($responses[$j]['idUser']));
+								$responseUser = $stmt->fetch();
+							?>
+								<p>Written by <?=$responseUser['name']?></p>
+								<li>
+									<?=$responses[$j]['text']?>
+								</li>
+							<?php } ?>
+						</ul>
+					</li>
 				<?php } ?>
 			</ul>
 		</div>
