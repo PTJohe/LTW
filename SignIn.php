6376ba114cd5f45bcf32af12a1388072602e5762
FranciscoBarbosa <franciscolamares@gmail.com>
2016-12-05 14:52:25 +0000

    Login verifica Conta corretamente

--- SignIn.php
+++ SignIn.php
@@ -4,34 +4,38 @@
 $dbh=new PDO('sqlite:database.db');
 $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
-<!-- foreach( $usernames as $row){
-  echo $row['username'];
-} -->
+
 <head>
 <title>Sign In</title>
 <meta charset="utf-8">
+</head>
 <body>
   <div id="container form-signin">
-  <?php  $stmt=$dbh->prepare('SELECT username FROM users ');
-    $stmt->execute();
-    $usernames=$stmt->fetchAll();
-    $inputUserName=$_POST['uname'];
+  <?php  $stmt=$dbh->prepare('SELECT * FROM users WHERE username = ? AND password = ?');
+
+    $inputUsername=$_POST['uname'];
     $inputPassword=$_POST['psw'];
+
+    if(isset($_POST['login'])){
+      $stmt->execute(array($inputUsername,$inputPassword));
+      $account=$stmt->fetchAll();
+    }
+
     $bool=0;
-    foreach( $usernames as $row){
-      if(isset($_POST['login']) && $row['username']==$inputUserName &&
+    foreach($account as $row){
+      if(isset($_POST['login']) && $row['username']==$inputUsername &&
       $row['password']==$inputPassword){
         $bool=1;
         echo 'Login Successful';
       }
   }
-    if(bool==0){
+    if(isset($_POST['login'])&& bool==0){
       echo 'wrong Password or Username';
     }
   ?>
 </div>
   <div id="container">
-    <form  class="form-signin" role = "form"  action = "<?php echo $_SERVER['PHP_SELF']; ?>"
+    <form method="post" class="form-signin" role = "form" action = "<?php echo $_SERVER['PHP_SELF']; ?>"
       method = "post">
     <label><b>Username</b>
     <input class="UserName" type="text" placeholder="Enter Username" name="uname" required>
