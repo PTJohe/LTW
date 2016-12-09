2e18d3bb408e3fa91ac6bcdfa6efb34bd09e2310
PTJohe <jharaujo95@gmail.com>
2016-12-09 21:47:19 +0000

    Fixed Homepage css.

--- Foodle/HomePage.php
+++ Foodle/HomePage.php
@@ -4,29 +4,32 @@ session_start();
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Home</title>
-  <meta charset="utf-8">
-  <link rel="stylesheet">
-  <!-- href="HomePage.css"> -->
+	<title>Home</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="HomePage.css">
 </head>
 
 <body>
-  <header>
-  <?php include 'header.php' ?>
-    <nav>
-      <?php include 'nav.php' ?>
-    </nav>
-    <form  class="form-search" role = "form" action = "Restaurant_Search.php" method="post">
-      <p class="Search"><input type="text" name="search" placeholder="Search Restaurants by name, location,food,menu"></p>
-      <button type="submit" name="searchBtn">Search</button>
-    </form>
-  </header>
+	<header>
+		<?php include 'header.php' ?>
+		<nav>
+			<?php include 'nav.php' ?>
+		</nav>
+	</header>
 
-  <div class="Colecoes">
-    <h2>Curiosities</h2>
-    <img src="topSemana.jpg" alt="Image" style="width:104px;height:58px;">Top of the Week
-    <p><img src="./resources/restaurantCur.jpg" alt="Image" style="width:104px;height:58px;">News</p>
-  </div>
+	<div id="SearchBar">
+		<form  class="form-search" role = "form" action = "Restaurant_Search.php" method="post">
+			<p class="Search">
+				<input type="text" name="search" placeholder="Search Restaurants by name, location,food,menu">
+				<button type="submit" name="searchBtn">Search</button>
+			</p>
+		</form>
+	</div>
+	<div id="Colecoes">
+		<h2>Curiosities</h2>
+		<img src="topSemana.jpg" alt="Image" style="width:104px;height:58px;">Top of the Week
+		<p><img src="./resources/restaurantCur.jpg" alt="Image" style="width:104px;height:58px;">News</p>
+	</div>
 </body>
 
 </html>
