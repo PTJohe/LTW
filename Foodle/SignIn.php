310d46e369894677752c3e7f8741d0d87f8ec2f3
FranciscoBarbosa <franciscolamares@gmail.com>
2016-12-14 22:52:05 +0000

    login css almost done

--- Foodle/SignIn.php
+++ Foodle/SignIn.php
@@ -7,23 +7,45 @@ session_start();
 <head>
     <title>Sign In</title>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="css/Login.css">
 </head>
 <body>
-  <div id="container">
-    <form  class="form-signin" role = "form"
-    action = "action_login.php" method="post">
-    <label><b>Username</b>
+  <header>
+		<?php include 'header.php' ?>
+  </header>
+  <div id="main">
+    <div id="LoginFoodle">
+      <h1>Login into </h1><h1 id="foodle">foodle</h1><h1>!</h1>
+    </div>
+    <div id=Inputs>
+    <form  class="form-signin" role = "form" action = "action_login.php" method="post">
+<p>
+      <div id=username>
+        <form class="form-user">
+    <label ><b>Username</b>
         <input class="UserName" type="text" placeholder="Enter Username" name="uname" required>
     </label>
+  </form>
+  </div>
+</p>
     <p>
+      <div id=password>
+        <form class="form-pass">
         <label><b>Password</b>
             <input class="Password"type="password" placeholder="Enter Password" name="psw" required>
         </label>
+      </form>
+      </div>
     </p>
-    <p>
+    <div id=submit>
         <button type="submit" name="login">Sign In</button>
-    </p>
+  </div>
+  </div>
+  </div>
+    <footer>
+      <?php include 'footer.php' ?>
+    </footer>
 </form>
-</div>
+
 </body>
 </html>
