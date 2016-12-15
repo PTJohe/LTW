cd48f2a3693dea56912e3a5247ddbd9d40f2f880
ZeCarlosCoutinho <jczelik@gmail.com>
2016-12-15 09:30:38 +0000

    Last edit restaurant page

--- Foodle/restaurant/EditRestaurantPage.php
+++ Foodle/restaurant/EditRestaurantPage.php
@@ -74,97 +74,58 @@ $restaurantCreationDate = $selectedRestaurant['creationDate'];
 <head>
 	<meta charset="UTF-8">
 	<title><?=$restaurantName?></title>
+	<link rel="stylesheet" href="../css/restaurant/RestaurantPage.css">
 	<script src="../js/lib/jquery-1.11.3.min.js"></script>
 	<script src="../js/editRestaurantInfo.js"></script>
 </head>
 
 <body>
-	<header>
-		<?php include '../header.php' ?>
-	</header>
-
-	<div id="restaurantDetails">
-		<input class="restaurantId" type="hidden" value=<?=$restaurantId?>>
-		<input class="doneButton" type="button" value="Done editing"><br>
-		<img src="<?=$restaurantLogo?>" alt=<?=$restaurantName?> width="300" height="100">
-		<!-- TODO Upload Logo -->
-
-		<div class="editable" id="restaurantName">
-			<h2 class="editItem"><?=$restaurantName?></h2>
-			<input class="editButton" type="image" src="<?=$resourcesPath?>editIcon.png" alt="Edit" width="25" height="25">
-			<input class="checkButton" type="hidden" src="<?=$resourcesPath?>checkEditIcon.png" alt="Check" width="25" height="25">
-			<input class="cancelButton" type="hidden" src="<?=$resourcesPath?>cancelEditIcon.png" alt="Cancel" width="25" height="25">
-		</div>
-
-		<div class="editable" id="restaurantAddress">
-			<p>Address:</p>
-			<p class="editItem"><?=$restaurantAddress?></p>
-			<input class="editButton" type="image" src="<?=$resourcesPath?>editIcon.png" alt="Edit" width="25" height="25">
-			<input class="checkButton" type="hidden" src="<?=$resourcesPath?>checkEditIcon.png" alt="Check" width="25" height="25">
-			<input class="cancelButton" type="hidden" src="<?=$resourcesPath?>cancelEditIcon.png" alt="Cancel" width="25" height="25">
-		</div>
-		<div class="editable" id="restaurantNumber">
-			<p>Number:</p>
-			<p class="editItem"><?=$restaurantContact?></p>
-			<input class="editButton" type="image" src="<?=$resourcesPath?>editIcon.png" alt="Edit" width="25" height="25">
-			<input class="checkButton" type="hidden" src="<?=$resourcesPath?>checkEditIcon.png" alt="Check" width="25" height="25">
-			<input class="cancelButton" type="hidden" src="<?=$resourcesPath?>cancelEditIcon.png" alt="Cancel" width="25" height="25">
-		</div>
-		<p>Rating: <?=$restaurantAverageRating?> / 5 </p>
-		<div class="editable" id="restaurantDescription">
-			<p class="editItem"><?=$restaurantDescription?></p>
-			<input class="editButton" type="image" src="<?=$resourcesPath?>editIcon.png" alt="Edit" width="25" height="25">
-			<input class="checkButton" type="hidden" src="<?=$resourcesPath?>checkEditIcon.png" alt="Check" width="25" height="25">
-			<input class="cancelButton" type="hidden" src="<?=$resourcesPath?>cancelEditIcon.png" alt="Cancel" width="25" height="25">
-		</div>
-		<div class="editableList" id="restaurantCategory">
-			<p>Category:</p>
-			<p class="editItem"><?=$restaurantCategory?></p>
-		</div>
-		<p>Creation Date: <?=$restaurantCreationDate?></p>
+	<div id="headerdiv">
+		<header>
+			<?php include '../header.php'; ?>
+		</header>
 	</div>
-	<div id="reviews">
-		<ul>
-			<?php for($i = 0; $i < count($restaurantReviews); $i++){ 
-				//Gets the user who wrote the actual review
-				$stmt = $dbh->prepare('SELECT * FROM users WHERE idUser = ?');
-				$stmt->execute(array($restaurantReviews[$i]['idUser']));
-				$reviewUser = $stmt->fetch();
-				$reviewId = $restaurantReviews[$i]['idReview'];
-				?>
-
-				<div class="review">
-					<p>Rating: <?=$restaurantReviews[$i]['rating']?></p>
-
-					<p>Written by <?=$reviewUser['name'];?></p>
-					<li>
-						<?=$restaurantReviews[$i]['text']?>
-						<ul class="responses" id="response<?=$reviewId?>">
-							<?php 
-							//Get the users who wrote the responses
-							$stmt = $dbh->prepare('SELECT * FROM responses WHERE idReview = ?');
-							$stmt->execute(array($reviewId));
-							$responses = $stmt->fetchAll();
-							
-							for($j = 0; $j < count($responses); $j++){
-								$stmt = $dbh->prepare('SELECT * FROM users WHERE idUser = ?');
-								$stmt->execute(array($responses[$j]['idUser']));
-								$responseUser = $stmt->fetch();
-								?>
-								<div class="response">
-									<p>Written by <?=$responseUser['name']?></p>
-									<li>
-										<?=$responses[$j]['text']?>
-									</li>
-								</div>
-								<?php 
-							} ?>
-						</ul>
-					</li>
-				</div>
-				<?php 
-			} ?>
-		</ul>
+	<div id="main">
+		<div id="restaurantDetails">
+			<input class="restaurantId" type="hidden" value=<?=$restaurantId?>>
+			<input class="doneButton" type="button" value="Done editing"><br>
+			<img id="logo" src="<?=$restaurantLogo?>" alt=<?=$restaurantName?> width="300" height="100">
+			<!-- TODO Upload Logo -->
+	
+			<div class="editable" id="restaurantName">
+				<h2 class="editItem"><?=$restaurantName?></h2>
+				<input class="editButton" type="image" src="<?=$resourcesPath?>editIcon.png" alt="Edit" width="25" height="25">
+				<input class="checkButton" type="hidden" src="<?=$resourcesPath?>checkEditIcon.png" alt="Check" width="25" height="25">
+				<input class="cancelButton" type="hidden" src="<?=$resourcesPath?>cancelEditIcon.png" alt="Cancel" width="25" height="25">
+			</div>
+	
+			<div class="editable" id="restaurantAddress">
+				<p>Address:</p>
+				<p class="editItem"><?=$restaurantAddress?></p>
+				<input class="editButton" type="image" src="<?=$resourcesPath?>editIcon.png" alt="Edit" width="25" height="25">
+				<input class="checkButton" type="hidden" src="<?=$resourcesPath?>checkEditIcon.png" alt="Check" width="25" height="25">
+				<input class="cancelButton" type="hidden" src="<?=$resourcesPath?>cancelEditIcon.png" alt="Cancel" width="25" height="25">
+			</div>
+			<div class="editable" id="restaurantNumber">
+				<p>Number:</p>
+				<p class="editItem"><?=$restaurantContact?></p>
+				<input class="editButton" type="image" src="<?=$resourcesPath?>editIcon.png" alt="Edit" width="25" height="25">
+				<input class="checkButton" type="hidden" src="<?=$resourcesPath?>checkEditIcon.png" alt="Check" width="25" height="25">
+				<input class="cancelButton" type="hidden" src="<?=$resourcesPath?>cancelEditIcon.png" alt="Cancel" width="25" height="25">
+			</div>
+			<p>Rating: <?=$restaurantAverageRating?> / 5 </p>
+			<div class="editable" id="restaurantDescription">
+				<p class="editItem"><?=$restaurantDescription?></p>
+				<input class="editButton" type="image" src="<?=$resourcesPath?>editIcon.png" alt="Edit" width="25" height="25">
+				<input class="checkButton" type="hidden" src="<?=$resourcesPath?>checkEditIcon.png" alt="Check" width="25" height="25">
+				<input class="cancelButton" type="hidden" src="<?=$resourcesPath?>cancelEditIcon.png" alt="Cancel" width="25" height="25">
+			</div>
+			<div class="editableList" id="restaurantCategory">
+				<p>Category:</p>
+				<p class="editItem"><?=$restaurantCategory?></p>
+			</div>
+			<p>Creation Date: <?=$restaurantCreationDate?></p>
+		</div>
 	</div>
 </body>
 </html>
